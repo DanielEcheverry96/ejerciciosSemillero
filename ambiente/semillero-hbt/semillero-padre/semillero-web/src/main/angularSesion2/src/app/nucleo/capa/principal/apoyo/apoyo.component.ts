b1a7f685107b2992c48393d53b864452e1e61173
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2019-04-09 02:03:02 -0500

    Se agrega angularSesion2

---
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angularSesion2/src/app/nucleo/capa/principal/apoyo/apoyo.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { PersonaDTO } from './modelo/personaDTO';
+
+@Component({
+  selector: 'app-apoyo',
+  templateUrl: './apoyo.component.html',
+  styleUrls: []
+})
+export class ApoyoComponent implements OnInit {
+  public mostrarFormulario: boolean;
+  public persona: PersonaDTO;
+  public personas: PersonaDTO[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.persona = {
+      id: '0',
+      nombre: '',
+      apellido: '',
+      tipoIdentificacion: '',
+      numeroIdentificacion: '',
+      mayorEdad: false,
+      sexo: ''
+    };
+
+    this.personas = [];
+    this.mostrarFormulario = true;
+  }
+
+  mostrar(): void {
+  }
+
+  ocultar(): void {
+
+  }
+
+  guardar(): void {
+    this.personas.push(this.persona);
+    console.log('guardar()' + this.persona.nombre);
+  }
+
+  private borrar(): void {
+
+  }
+
+}
